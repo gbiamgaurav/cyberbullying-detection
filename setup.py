aa62bdfb27406133de607ffa2cf419ecc0f6954d
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-07-14 19:14:24 +0530

    setup and requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cyberbullying-detection",
+    version="0.0.1",
+    author="Gaurav",
+    author_email="gaurav.bhattacharya10@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
